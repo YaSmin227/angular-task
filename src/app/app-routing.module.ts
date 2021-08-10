4a55ff3d6ca74ae8a4e76b665195a168878d576c
YaSmin Tolba <63653381+YaSmin227@users.noreply.github.com>
2021-08-10 11:31:02 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:'customer',loadChildren:()=> import ('./customer/cutomer-routing.module').then(m=> m.CutomerRoutingModule)},
+  {path:'product', loadChildren:()=> import ('./products/products-routing.module').then(m=>m.ProductsRoutingModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
